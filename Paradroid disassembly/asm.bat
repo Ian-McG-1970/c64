1738f1129fcd26bb524895c71f3628be66199651
Ian-McG-1970 <81464953+Ian-McG-1970@users.noreply.github.com>
2025-07-14 01:18:12 +0100

    Delete Paradroid disassembly directory

--- Paradroid disassembly/asm.bat
+++
@@ -1,5 +0,0 @@
-..\64tass -Wall -Wno-implied-reg --cbm-prg -o bin/pd.prg -L bin/list-co1.txt -l bin/labels.txt src/paradroid.asm
-	
-..\64tass -Wall -Wno-implied-reg --cbm-prg -o bin/data.prg src/data.asm
-	
-..\exomizer sfx 0x1000 bin/pd.prg bin/data.prg,0xe000 -n -o bin/paradroid.prg
